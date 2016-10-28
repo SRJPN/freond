980fc3990f9bebaed1dec31267c82050920ec5f7
Sooraj Parameswaran <soorajp@thoughtworks.com>
2016-10-28 22:46:43 +0530

    Sooraj: Simple Ticket

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+var urlParser = require('url-parse');
 
 var app = express();
 
@@ -8,5 +10,13 @@ app.use(function(req, res, next) {
 });
 
 app.use(express.static('./public'));
+
+app.get('/getTicket?', function(req, res, next) {
+    var urlObj = urlParser.qs.parse(req.url);
+    var file = fs.readFileSync('./public/ticket.html', 'utf8');
+    file = file.replace('_to_', urlObj.to)
+    file = file.replace('_from_', urlObj.from)
+    res.end(file)
+});
 
 module.exports = app;
